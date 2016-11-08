2f4cd06d56a5864ba3ff7b7f171ff2d2f132cc9d
Avi Blinder <aviblinder@gmail.com>
2016-11-08 21:24:34 +0200

    script commit

--- scripts/bots_main.r
+++ scripts/bots_main.r
@@ -4,3 +4,14 @@ users <- read.csv("D:/Yelp/r_datasets//users.csv",stringsAsFactors = FALSE)
 
 business <- read.csv("D:/Yelp/r_datasets/business.csv",header = TRUE,stringsAsFactors = FALSE)
 reviews <- read.csv("D:/Yelp/r_datasets/reviews.csv",header = TRUE,stringsAsFactors = FALSE)
+
+
+names(users)
+names(reviews)
+
+library(dplyr)
+users %>% filter(FriendsNumber == 0 & fans <= 10) -> bot_users
+reviews %>% filter(user_id %in% bot_users$user_id) %>%
+  group_by (business_id) %>% mutate(reviews_count = count(business_id)) -> 
+  filtered_reviews
+
